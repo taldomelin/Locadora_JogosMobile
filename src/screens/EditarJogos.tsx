c437c7b53296e4e106b1cc446de800365434dbf3
taldomelin <gumelo.cant@gmail.com>
2024-06-04 15:47:34 -0300

    Atualizando git

--- src/screens/EditarJogos.tsx
+++ src/screens/EditarJogos.tsx
@@ -45,66 +45,66 @@ const EditarJogos: React.FC<Props> = ({ route }) => {
     const newErrors = {};
 
     if (!nome) {
-      newErrors.nome = "O campo 'Nome' é obrigatório.";
+      newErrors.nome = "O campo nome é obrigatório.";
       hasErrors = true;
     } else if (nome.length < 5 || nome.length > 120) {
-      newErrors.nome = "O campo 'Nome' deve ter entre 5 e 120 caracteres.";
+      newErrors.nome = "O campo nome deve ter entre 5 e 120 caracteres.";
       hasErrors = true;
     }
 
     if (!preco) {
-      newErrors.preco = "O campo 'Preço' é obrigatório.";
+      newErrors.preco = "O campo preço é obrigatório";
       hasErrors = true;
-    } else if (!/^\d+(\.\d{1,2})?$/.test(preco)) {
-      newErrors.preco = "O campo 'Preço' deve ser um número decimal com no máximo 2 casas decimais.";
+    } else if (!/^\d+(\.\d{2})$/.test(preco)) {
+      newErrors.preco = "O campo preço deve ter 2 casas decimais";
       hasErrors = true;
     }
 
     if (!descricao) {
-      newErrors.descricao = "O campo 'Descrição' é obrigatório.";
+      newErrors.descricao = "O campo descrição é obrigatório.";
       hasErrors = true;
     } else if (descricao.length < 10 || descricao.length > 800) {
-      newErrors.descricao = "O campo 'Descrição' deve ter entre 10 e 800 caracteres.";
+      newErrors.descricao = "O campo descrição deve ter entre 10 e 800 caracteres.";
       hasErrors = true;
     }
 
     if (!classificacao) {
-      newErrors.classificacao = "O campo 'Classificação' é obrigatório.";
+      newErrors.classificacao = "O campo classificação é obrigatório.";
       hasErrors = true;
     } else if (classificacao.length < 5 || classificacao.length > 20) {
-      newErrors.classificacao = "O campo 'Classificação' deve ter entre 5 e 20 caracteres.";
+      newErrors.classificacao = "O campo classificação deve ter entre 5 e 20 caracteres.";
       hasErrors = true;
     }
 
     if (!plataformas) {
-      newErrors.plataformas= "O campo 'Plataformas' é obrigatório.";
+      newErrors.plataformas= "O campo plataformas é obrigatório.";
       hasErrors = true;
     } else if (plataformas.length < 3 || plataformas.length > 60) {
-      newErrors.plataformas = "O campo 'Plataformas' deve ter entre 3 e 60 caracteres.";
+      newErrors.plataformas = "O campo plataformas deve ter entre 3 e 60 caracteres.";
       hasErrors = true;
     }
 
     if (!desenvolvedor) {
-      newErrors.desenvolvedor = "O campo 'Desenvolvedor' é obrigatório.";
+      newErrors.desenvolvedor = "O campo desenvolvedor é obrigatório.";
       hasErrors = true;
     } else if (desenvolvedor.length < 2 || desenvolvedor.length > 120) {
-      newErrors.desenvolvedor = "O campo 'Desenvolvedor' deve ter entre 2 e 120 caracteres.";
+      newErrors.desenvolvedor = "O campo desenvolvedor deve ter entre 2 e 120 caracteres.";
       hasErrors = true;
     }
 
     if (!distribuidora) {
-      newErrors.distribuidora = "O campo 'Distribuidora' é obrigatório.";
+      newErrors.distribuidora = "O campo distribuidora é obrigatório.";
       hasErrors = true;
     } else if (distribuidora.length < 2 || distribuidora.length > 120) {
-      newErrors.distribuidora = "O campo 'Distribuidora' deve ter entre 2 e 120 caracteres.";
+      newErrors.distribuidora = "O campo distribuidora deve ter entre 2 e 120 caracteres.";
       hasErrors = true;
     }
 
     if (!categoria) {
-      newErrors.categoria = "O campo 'Categoria' é obrigatório.";
+      newErrors.categoria = "O campo categoria é obrigatório.";
       hasErrors = true;
     } else if (categoria.length < 3 || categoria.length > 55) {
-      newErrors.categoria = "O campo 'Categoria' deve ter entre 3 e 55 caracteres.";
+      newErrors.categoria = "O campo categoria deve ter entre 3 e 55 caracteres.";
       hasErrors = true;
     }
 
